04e0683f21051f2722745c27a274972f5c60f002
muh ikhsan <thisisli96@yahoo.com>
2020-02-15 09:04:24 +0700

    POINT 2 make find user // make recycle view

--- app/src/main/java/com/example/whatsappclone/FindUserActivity.java
+++ app/src/main/java/com/example/whatsappclone/FindUserActivity.java
@@ -1,14 +1,38 @@
 package com.example.whatsappclone;
 
+import androidx.appcompat.app.AlertController;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class FindUserActivity extends AppCompatActivity {
 
+    private RecyclerView mUserList;
+    private RecyclerView.Adapter mUserListAdapter;
+    private RecyclerView.LayoutManager mUSerListLayoutManager;
+
+    ArrayList<UserListAdapter> userList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
-    }
+        userList = new ArrayList<>();
+        initializeRecylerView();
+    }// akhir oncreate
+
+    private void initializeRecylerView() {
+
+        mUserList = findViewById(R.id.userList);
+        mUserList.setNestedScrollingEnabled(false);
+        mUserList.setHasFixedSize(false);
+        mUSerListLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayout.VERTICAL , false);
+        mUserList.setLayoutManager(mUSerListLayoutManager);
+        mUserListAdapter = new UserListAdapter(userList);
+        mUserList.setAdapter(mUserListAdapter);
+    } // akhir inicial
 }
